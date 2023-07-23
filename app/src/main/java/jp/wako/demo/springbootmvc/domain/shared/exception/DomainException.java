aea4922c114cb84999469716e51a89ca44afed8b
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-07-23 08:38:19 +0000

    feat: DomainExceptionの導入

---
+++ app/src/main/java/jp/wako/demo/springbootmvc/domain/shared/exception/DomainException.java
@@ -0,0 +1,13 @@
+package jp.wako.demo.springbootmvc.domain.shared.exception;
+
+public class DomainException extends RuntimeException {
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+    public DomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
