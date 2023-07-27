34374cc342ecee68e6f014cace755aa4af68a1de
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-07-27 03:56:59 +0000

    refactor(infra.tasks): InMemoryRepositoryがDAOに依存しないように修正
    DAOの導入時の試行錯誤にこっちを直すとリポジトリでエラー、あっちを直すとリポジトリでエラーってなって邪魔だったから

--- app/src/main/java/jp/wako/demo/springbootmvc/infra/tasks/InMemoryTaskRepository.java
+++ app/src/main/java/jp/wako/demo/springbootmvc/infra/tasks/InMemoryTaskRepository.java
@@ -1,66 +1,64 @@
 package jp.wako.demo.springbootmvc.infra.tasks;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
 import jp.wako.demo.springbootmvc.domain.tasks.ITaskRepository;
 import jp.wako.demo.springbootmvc.domain.tasks.Task;
-import jp.wako.demo.springbootmvc.infra.tasks.dao.ITaskEntityDao;
-import jp.wako.demo.springbootmvc.infra.tasks.dao.InMemoryTaskEntityDao;
-import jp.wako.demo.springbootmvc.infra.tasks.dao.TaskEntity;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Repository
 public final class InMemoryTaskRepository implements ITaskRepository {
 
-    private final ITaskEntityDao dao = new InMemoryTaskEntityDao();
+    // TODO: Setにする
+    private final List<Task> tasks = new ArrayList<>() {{
+        add(Task.reconstruct("1", "#3245 Replace InMemory repository with Postgres", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 10, 00)));
+        add(Task.reconstruct("2", "#3246 Change the structure of the VM for a presentation layer task", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 10, 30)));
+        add(Task.reconstruct("3", "#3247 Hide task card description", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 11, 00)));
+    }};
 
     public List<Task> findAll() {
+        return this.tasks;
+    }
 
-        var foundTaskEntities = this.dao.findAll();
-        var foundTasks = foundTaskEntities
-            .stream()
-            .map(this::convertEntityToDomain)
-            .collect(Collectors.toList());
-
-        return foundTasks;
+    public void delete(final String id) {
+        this.tasks.removeIf(addedTask -> addedTask.getId().equals(id));
     }
 
     public Optional<Task> findBy(final String id) {
 
-        var maybeTaskEntity = this.dao.findBy(id);
-        var maybeTask = maybeTaskEntity.map(this::convertEntityToDomain);
+        var foundTask = this.tasks
+            .stream()
+            .filter(addedTask -> addedTask.getId().equals(id))
+            .findFirst();
 
-        return maybeTask;
-    }
-
-    private Task convertEntityToDomain(final TaskEntity taskEntity) {
-        return Task.reconstruct(
-            taskEntity.getId(),
-            taskEntity.getTitle(),
-            taskEntity.getDescription(),
-            taskEntity.getCreateAt());
+        return foundTask;
     }
 
     public void save(final Task task) {
-        var taskEntity = convertDomainToEntity(task);
-        this.dao.save(taskEntity);
-    }
 
-    private TaskEntity convertDomainToEntity(final Task task) {
-        return new TaskEntity(
-            task.getId(),
-            task.getTitle(),
-            task.getDescription(),
-            task.getCreateAt());
-    }
+        // 保存しようとしているタスクが存在しているかチェック
+        var exists = this.tasks
+            .stream()
+            .anyMatch(addedTask -> addedTask.getId().equals(task.getId()));
 
-    public void delete(final String id) {
-        this.dao.delete(id);
+        // 存在しない場合はそのまま追加
+        if (!exists) {
+            this.tasks.add(task);
+            return;
+        }
+
+        // 存在する場合は置き換える
+        this.tasks.replaceAll((addedTask) -> {
+            if (addedTask.getId().equals(task.getId())) {
+                return task;
+            }
+            return addedTask;
+        });
+
     }
 
 }
