1f33537b632f9ee043550b4536ce0a22ea619c2b
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-07-31 10:15:58 +0000

    refactor(infra.tasks): インメモリなリポジトリが保持するクラスをEntityに変更する修正

--- app/src/main/java/jp/wako/demo/springbootmvc/infra/tasks/InMemoryTaskRepository.java
+++ app/src/main/java/jp/wako/demo/springbootmvc/infra/tasks/InMemoryTaskRepository.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
 import jp.wako.demo.springbootmvc.domain.tasks.TaskRepository;
+import jp.wako.demo.springbootmvc.infra.tasks.dao.TaskEntity;
 import jp.wako.demo.springbootmvc.domain.tasks.Task;
 import lombok.RequiredArgsConstructor;
 
@@ -15,52 +17,70 @@ import lombok.RequiredArgsConstructor;
 @Repository
 public class InMemoryTaskRepository implements TaskRepository {
 
-    // TODO: Setにする
-    private final List<Task> tasks = new ArrayList<>() {{
-        add(Task.reconstruct(1, "#3245 Replace InMemory repository with Postgres", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 10, 00)));
-        add(Task.reconstruct(2, "#3246 Change the structure of the VM for a presentation layer task", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 10, 30)));
-        add(Task.reconstruct(3, "#3247 Hide task card description", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 11, 00)));
+    private final List<TaskEntity> taskEntites = new ArrayList<>() {{
+        add(new TaskEntity(1, "#3245 Replace InMemory repository with Postgres", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 10, 00)));
+        add(new TaskEntity(2, "#3246 Change the structure of the VM for a presentation layer task", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 10, 30)));
+        add(new TaskEntity(3, "#3247 Hide task card description", "This is a task added for testing purposes.", LocalDateTime.of(2023, 07, 23, 11, 00)));
     }};
 
     public List<Task> findAll() {
-        return this.tasks;
-    }
 
-    public void delete(final String id) {
-        this.tasks.removeIf(addedTask -> addedTask.getId().equals(id));
+        var tasks = taskEntites
+            .stream()
+            .map(this::convertToDomain)
+            .collect(Collectors.toList());
+
+        return tasks;
     }
 
     public Optional<Task> findBy(final String id) {
 
-        var foundTask = this.tasks
+        var foundTaskEntity = this.taskEntites
             .stream()
-            .filter(addedTask -> addedTask.getId().equals(id))
+            .filter(taskEntity -> taskEntity.getId().equals(id))
             .findFirst();
 
-        return foundTask;
+        var maybeTask = foundTaskEntity.map(this::convertToDomain);
+        return maybeTask;
+    }
+
+    private Task convertToDomain(final TaskEntity taskEntity) {
+        return Task.reconstruct(
+            taskEntity.getId(),
+            taskEntity.getTitle(),
+            taskEntity.getDescription(),
+            taskEntity.getCreateAt());
     }
 
     public void save(final Task task) {
 
-        // 保存しようとしているタスクが存在しているかチェック
-        var exists = this.tasks
-            .stream()
-            .anyMatch(addedTask -> addedTask.getId().equals(task.getId()));
+        if (task.getId() == null) {
 
-        // 存在しない場合はそのまま追加
-        if (!exists) {
-            this.tasks.add(task);
-            return;
+            var taskEntity = convertToEntity(task);
+            this.taskEntites.add(taskEntity);
+
+        } else {
+
+            this.taskEntites.replaceAll(taskEntity -> {
+                if (taskEntity.getId().equals(task.getId())) {
+                    return convertToEntity(task);
+                }
+                return taskEntity;
+            });
+
         }
+    }
 
-        // 存在する場合は置き換える
-        this.tasks.replaceAll((addedTask) -> {
-            if (addedTask.getId().equals(task.getId())) {
-                return task;
-            }
-            return addedTask;
-        });
+    private TaskEntity convertToEntity(final Task task) {
+        return new TaskEntity(
+            this.taskEntites.size() + 1,
+            task.getTitle(),
+            task.getDescription(),
+            task.getCreateAt());
+    }
 
+    public void delete(final String id) {
+        this.taskEntites.removeIf(addedTask -> addedTask.getId().equals(id));
     }
 
 }
