34b16a26d37672c3b9aa3658c40f86631cbf6b91
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-08-02 23:20:00 +0000

    refactor(usecase.tasks): 追加/更新系のユースケースに@Transactionalを付与する修正

--- app/src/main/java/jp/wako/demo/springbootmvc/usecase/tasks/add/AddTaskUseCase.java
+++ app/src/main/java/jp/wako/demo/springbootmvc/usecase/tasks/add/AddTaskUseCase.java
@@ -1,6 +1,7 @@
 package jp.wako.demo.springbootmvc.usecase.tasks.add;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.wako.demo.springbootmvc.domain.tasks.TaskRepository;
 import jp.wako.demo.springbootmvc.domain.tasks.Task;
@@ -12,6 +13,7 @@ public final class AddTaskUseCase {
 
     private final TaskRepository repository;
 
+    @Transactional
     public AddTaskResponse execute(final AddTaskRequest request) {
 
         var task = Task.create(request.getTitle());
--- app/src/main/java/jp/wako/demo/springbootmvc/usecase/tasks/update/UpdateTaskUseCase.java
+++ app/src/main/java/jp/wako/demo/springbootmvc/usecase/tasks/update/UpdateTaskUseCase.java
@@ -1,6 +1,7 @@
 package jp.wako.demo.springbootmvc.usecase.tasks.update;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.wako.demo.springbootmvc.domain.shared.exception.DomainException;
 import jp.wako.demo.springbootmvc.domain.tasks.TaskRepository;
@@ -12,6 +13,7 @@ public final class UpdateTaskUseCase {
 
     private final TaskRepository repository;
 
+    @Transactional
     public UpdateTaskResponse execute(UpdateTaskRequest request) {
     
         var maybeTask = this.repository.findBy(Integer.parseInt(request.getId()));
