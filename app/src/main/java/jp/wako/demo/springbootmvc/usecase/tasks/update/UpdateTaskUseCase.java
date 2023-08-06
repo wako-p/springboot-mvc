7a209efb366207e6c53000bd15b8021ad227acdc
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-08-06 09:17:05 +0000

    chore: 楽観ロックについてのコメントを追加

--- app/src/main/java/jp/wako/demo/springbootmvc/usecase/tasks/update/UpdateTaskUseCase.java
+++ app/src/main/java/jp/wako/demo/springbootmvc/usecase/tasks/update/UpdateTaskUseCase.java
@@ -20,6 +20,9 @@ public class UpdateTaskUseCase {
         var foundTask = maybeTask
             .orElseThrow(() -> new UseCaseException("Task(id:" + request.getId() + ") not found."));
 
+        // NOTE: 更新前にリクエストのバージョンと取得したTaskのバージョン比較しないとダメなのでは？？？
+        // NOTE: バージョンの比較ロジック(インフラ層の責務？)をユースケースに持ち込むのはいかがなものか？
+
         foundTask.updateTitle(request.getTitle());
         foundTask.updateDescription(request.getDescription());
         var updatedTaskId = this.repository.save(foundTask);
