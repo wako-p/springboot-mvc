bc2c583353a3ba81df701aed303c154592297650
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-08-06 07:09:06 +0000

    feat(presentation): テキトーエラーページの作成

--- app/src/main/java/jp/wako/demo/springbootmvc/presentation/shared/exception/ExceptionHandler.java
+++ app/src/main/java/jp/wako/demo/springbootmvc/presentation/shared/exception/ExceptionHandler.java
@@ -1,28 +1,55 @@
 package jp.wako.demo.springbootmvc.presentation.shared.exception;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import jp.wako.demo.springbootmvc.domain.shared.exception.DomainException;
 import jp.wako.demo.springbootmvc.usecase.shared.exception.UseCaseException;
 
-// TODO: 例外メッセージをエラー画面に渡せるようにする
 @ControllerAdvice
 public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(UseCaseException.class)
-    public String handleUseCaseException(final UseCaseException exception) {
-        return "/error";
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleUseCaseException(final Model model, final UseCaseException exception) {
+        return error(model, HttpStatus.BAD_REQUEST, exception);
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(DomainException.class)
-    public String handleDomainException(final DomainException exception) {
-        return "/error";
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleDomainException(final Model model, final DomainException exception) {
+        return error(model, HttpStatus.BAD_REQUEST, exception);
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
-    public String handleOtherException(final Exception exception) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleOtherException(final Model model, final Exception exception) {
+        return error(model, HttpStatus.INTERNAL_SERVER_ERROR, exception);
+    }
+
+    private String error(final Model model, final HttpStatus httpStatus, final Exception exception) {
+        // TODO: ログにスタックトレースを出力する
+        model.addAttribute("timestamp", LocalDateTime.now());
+        model.addAttribute("status", httpStatus.value());
+        model.addAttribute("error", httpStatus.getReasonPhrase());
+        model.addAttribute("exception", exception.getClass());
+        model.addAttribute("message", exception.getMessage());
+        model.addAttribute("trace", getStackTrace(exception));
+        model.addAttribute("path", null);
         return "/error";
+    }
+
+    private String getStackTrace(Exception ex) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        return sw.toString();
     }
 
 }
