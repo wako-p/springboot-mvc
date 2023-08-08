761c312814beae5b1c93527fcc7716ea997a158f
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-08-08 11:00:10 +0000

    test(domain.tasks): タスクの日時に関連するテストを追加

--- app/src/test/java/jp/wako/demo/springbootmvc/domain/task/TaskTest.java
+++ app/src/test/java/jp/wako/demo/springbootmvc/domain/task/TaskTest.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import jp.wako.demo.springbootmvc.domain.shared.exception.DomainException;
 import jp.wako.demo.springbootmvc.domain.tasks.Task;
@@ -16,16 +17,35 @@ public final class TaskTest {
 
     @Nested
     class CreateTest {
+
         @Test
-        @DisplayName("引数にタイトルを指定して生成することができ、その値が属性として使用される。また、IDはnull、説明は空文字、バージョンは1となる。")
+        @DisplayName("引数にタイトルを指定して生成することができ、その値が属性として使用される。")
         void success1() {
             // when:
-            var task = Task.create("test1");
+            var task = Task.create("title");
 
             // then:
-            assertEquals("test1", task.getTitle());
+            assertEquals("title", task.getTitle());
+        }
+
+        @Test
+        @DisplayName("引数にタイトルを指定して生成することができ、IDはnull、説明は空文字、作成日/更新日は現在の日時、バージョンは1となる。")
+        void success2() {
+            // given:
+            // LocalDateTime.now()の返り値を固定化する
+            var datetime = LocalDateTime.of(2023, 07, 23, 11, 00);
+            var mock = Mockito.mockStatic(LocalDateTime.class);
+            mock.when(LocalDateTime::now).thenReturn(datetime);
+
+            // when:
+            var task = Task.create("title");
+            mock.close();
+
+            // then:
             assertEquals(null, task.getId());
             assertEquals("", task.getDescription());
+            assertEquals(datetime, task.getCreatedAt());
+            assertEquals(datetime, task.getUpdatedAt());
             assertEquals(1, task.getVersion());
         }
 
@@ -65,17 +85,30 @@ public final class TaskTest {
     class UpdateTest {
 
         @Test
-        @DisplayName("引数に指定したタイトルと説明で更新することができる。")
+        @DisplayName("引数に指定したタイトルと説明で更新することができる。また更新日はそのときの日時となる。")
         void success1() {
             // given:
-            var task = Task.reconstruct(999, "title", "description", LocalDateTime.of(2023, 07, 23, 10, 00), LocalDateTime.of(2023, 07, 23, 10, 00), 1);
+            // LocalDateTime.now()の返り値を固定化する
+            var datetime = LocalDateTime.of(2023, 07, 23, 11, 00);
+            var mock = Mockito.mockStatic(LocalDateTime.class);
+            mock.when(LocalDateTime::now).thenReturn(datetime);
+
+            var task = Task.reconstruct(
+                999,
+                "title",
+                "description",
+                LocalDateTime.of(2023, 07, 23, 10, 00),
+                LocalDateTime.of(2023, 07, 23, 10, 00), 
+                1);
 
             // when:
             task.update("updated title", "updated description");
+            mock.close();
 
             // then:
             assertEquals("updated title", task.getTitle());
             assertEquals("updated description", task.getDescription());
+            assertEquals(LocalDateTime.of(2023, 07, 23, 11, 00), task.getUpdatedAt());
         }
 
 
@@ -93,8 +126,6 @@ public final class TaskTest {
                 task.update("", "description");
             });
         }
-
-        // TODO: 更新日のテスト追加する
 
     }
 
