00ee725d3854e3f5363597a28097c3ae940a9505
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-08-13 06:12:09 +0000

    refactor(domain.shared): Entityは識別子によって識別されるべきなのでIDによって比較されるように修正

--- app/src/main/java/jp/wako/demo/springbootmvc/domain/shared/Entity.java
+++ app/src/main/java/jp/wako/demo/springbootmvc/domain/shared/Entity.java
@@ -3,10 +3,12 @@ package jp.wako.demo.springbootmvc.domain.shared;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode(of = {"id"})
 public class Entity {
     private final Integer id;
     private final LocalDateTime createdAt;
