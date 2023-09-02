8940806d8907388a070e93f46489aa91b6301a61
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-09-02 09:20:30 +0000

    feat(domain.projects): プロジェクトの追加

---
+++ app/src/main/java/jp/wako/demo/springbootmvc/domain/projects/Project.java
@@ -0,0 +1,73 @@
+package jp.wako.demo.springbootmvc.domain.projects;
+
+import java.time.LocalDateTime;
+
+import jp.wako.demo.springbootmvc.domain.shared.Entity;
+import jp.wako.demo.springbootmvc.domain.shared.exception.DomainException;
+import lombok.Getter;
+
+@Getter
+public final class Project extends Entity {
+
+    private String name;
+    private String description;
+
+    // TODO: あとで追加する
+    // private List<Task> tasks;
+
+    private Project(
+        final Integer id,
+        final String name,
+        final String description,
+        final LocalDateTime createdAt,
+        final LocalDateTime updatedAt,
+        final Integer version) {
+            super(id, createdAt, updatedAt, version);
+            this.name = name;
+            this.description = description;
+    }
+
+    public static Project create(final String name, final String description) {
+
+        if (!isValidName(name)) {
+            throw new DomainException("");
+        }
+
+        if (!isValidDescription(description)) {
+            throw new DomainException("");
+        }
+
+        return new Project(
+            null,
+            name,
+            description,
+            LocalDateTime.now().withNano(0),
+            LocalDateTime.now().withNano(0),
+            1);
+    }
+
+    private static boolean isValidName(final String name) {
+        if (name == null || name.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isValidDescription(final String description) {
+        if (description == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public static Project reconstruct(
+        final Integer id,
+        final String name,
+        final String description,
+        final LocalDateTime createAt,
+        final LocalDateTime updateAt,
+        final Integer version) {
+            return new Project(id, name, description, createAt, updateAt, version);
+    }
+
+}
---
+++ app/src/test/java/jp/wako/demo/springbootmvc/domain/projects/ProjectTest.java
@@ -0,0 +1,94 @@
+package jp.wako.demo.springbootmvc.domain.projects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import jp.wako.demo.springbootmvc.domain.shared.exception.DomainException;
+
+public class ProjectTest {
+    
+    @Nested
+    public class CreateTest {
+
+        static Stream<Arguments> parameterForSuccess1() {
+            return Stream.of(
+                Arguments.of("ProjectA", "This is a test project."),
+                Arguments.of("ProjectA", ""));
+        }
+
+        @ParameterizedTest
+        @MethodSource("parameterForSuccess1")
+        @DisplayName("引数にプロジェクト名と説明を指定して生成することができ、その値が属性として使用される。")
+        public void success1(final String name, final String description) {
+            // when:
+            var project = Project.create(name, description);
+
+            // then:
+            assertEquals(name, project.getName());
+            assertEquals(description, project.getDescription());
+        }
+
+        static Stream<Arguments> parameterForFailure1() {
+            return Stream.of(
+                Arguments.of(null, "This is a test project."),
+                Arguments.of("", "This is a test project."));
+        }
+
+        @ParameterizedTest
+        @MethodSource("parameterForFailure1")
+        @DisplayName("引数のプロジェクト名がnullまたは空文字の場合は例外がスローされる。")
+        public void failure1(final String name, final String description) {
+            // when/then:
+            assertThrows(DomainException.class, () -> {
+                Project.create(name, description);
+            });
+        }
+
+        @Test
+        @DisplayName("引数の説明がnullの場合は例外がスローされる。")
+        public void failure2() {
+            // when/then:
+            assertThrows(DomainException.class, () -> {
+                Project.create("ProjectA", null);
+            });
+        }
+
+    }
+
+    @Nested
+    class ReconstructTest {
+
+        @Test
+        @DisplayName("引数にID、プロジェクト名、説明などを指定して復元することができ、その値が属性として使用される。")
+        public void success1() {
+            // when:
+            var project = Project.reconstruct(
+                999,
+                "ProjectA",
+                "This is a test project.",
+                LocalDateTime.of(2023, 9, 2, 18, 00).withNano(0),
+                LocalDateTime.of(2023, 9, 2, 18, 00).withNano(0),
+                1);
+
+            // then:
+            assertEquals(999, project.getId());
+            assertEquals("ProjectA", project.getName());
+            assertEquals("This is a test project.", project.getDescription());
+            assertEquals(LocalDateTime.of(2023, 9, 2, 18, 00).withNano(0), project.getCreatedAt());
+            assertEquals(LocalDateTime.of(2023, 9, 2, 18, 00).withNano(0), project.getUpdatedAt());
+            assertEquals(1, project.getVersion());
+        }
+
+    }
+
+}
