d092ca5a2f13b2e7f97191bd3daa7e2f132a690a
shimokawabe masahiro <o9o76gp381w@gmail.com>
2024-09-16 04:52:34 +0000

    分割したコントローラー名のミス(逆だった)

--- app/src/main/java/jp/wako/demo/springbootmvc/presentation/controller/issues/IssueDetailsController.java
+++ app/src/main/java/jp/wako/demo/springbootmvc/presentation/controller/issues/IssueDetailsController.java
@@ -1,17 +1,31 @@
 package jp.wako.demo.springbootmvc.presentation.controller.issues;
 
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import jp.wako.demo.springbootmvc.presentation.controller.issues.viewmodel.IndexVM;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jp.wako.demo.springbootmvc.infra.shared.exception.PersistenceException;
+import jp.wako.demo.springbootmvc.presentation.controller.issues.viewmodel.CreateVM;
+import jp.wako.demo.springbootmvc.presentation.controller.issues.viewmodel.DetailVM;
+import jp.wako.demo.springbootmvc.presentation.controller.issues.viewmodel.EditVM;
 import jp.wako.demo.springbootmvc.presentation.controller.issues.viewmodel.Issue;
 import jp.wako.demo.springbootmvc.presentation.controller.issues.viewmodel.Project;
-import jp.wako.demo.springbootmvc.usecase.issues.search.IIssueSearchQuery;
-import jp.wako.demo.springbootmvc.usecase.issues.search.IssueSearchRequest;
+import jp.wako.demo.springbootmvc.usecase.issues.create.IssueCreateRequest;
+import jp.wako.demo.springbootmvc.usecase.issues.create.IssueCreateUseCase;
+import jp.wako.demo.springbootmvc.usecase.issues.delete.IssueDeleteRequest;
+import jp.wako.demo.springbootmvc.usecase.issues.delete.IssueDeleteUseCase;
+import jp.wako.demo.springbootmvc.usecase.issues.get.IssueGetRequest;
+import jp.wako.demo.springbootmvc.usecase.issues.get.IssueGetUseCase;
+import jp.wako.demo.springbootmvc.usecase.issues.update.IssueUpdateRequest;
+import jp.wako.demo.springbootmvc.usecase.issues.update.IssueUpdateUseCase;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -19,32 +33,130 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/projects/{projectId}")
 public class IssueDetailsController {
 
-    private final IIssueSearchQuery issueSearchQuery;
+    private final IssueGetUseCase issueGetUseCase;
+    private final IssueCreateUseCase issueCreateUseCase;
+    private final IssueUpdateUseCase issueUpdateUseCase;
+    private final IssueDeleteUseCase issueDeleteUseCase;
 
-    @ModelAttribute("indexVM")
-    public IndexVM createIndexVM() {
-        return new IndexVM();
+    @ModelAttribute("createVM")
+    private CreateVM createCreateVM() {
+        return new CreateVM();
     }
 
-    @GetMapping("/issues")
-    public String index(
+    @GetMapping("/issues/create")
+    public String create(
         @PathVariable final Integer projectId,
-        @ModelAttribute("indexVM") final IndexVM vm) {
+        @ModelAttribute("createVM") final CreateVM vm) {
+        vm.getProject().setId(projectId.toString());
+        return "/issues/create";
+    }
 
-        // TODO: UseCaseException補足する
-        var request = new IssueSearchRequest(projectId);
-        var response = this.issueSearchQuery.execute(request);
+    @PostMapping("/issues")
+    public String create(
+        @PathVariable final Integer projectId,
+        @ModelAttribute("createVM") @Validated final CreateVM vm,
+        final BindingResult bindingResult) {
 
-        var project = Project.createFrom(response.getProject());
-        var issues = response.getIssues()
-            .stream()
-            .map(Issue::createFrom)
-            .collect(Collectors.toList());
+        if (bindingResult.hasErrors()) {
+            return "/issues/create";
+        }
 
-        vm.setProject(project);
-        vm.setIssues(issues);
+        var request = new IssueCreateRequest(projectId, vm.getIssue().getTitle(), vm.getIssue().getDescription());
+        var response = this.issueCreateUseCase.execute(request);
 
-        return "/issues/index";
+        return "redirect:/projects/" + projectId + "/issues/" + response.getId();
+    }
+
+    @ModelAttribute("detailVM")
+    private DetailVM createDetailVM() {
+        return new DetailVM();
+    }
+
+    @GetMapping("/issues/{id}")
+    public String detail(
+        @PathVariable final Integer projectId,
+        @PathVariable final Integer id,
+        @ModelAttribute("detailVM") final DetailVM vm) {
+
+        var request = new IssueGetRequest(projectId, id);
+        var response = this.issueGetUseCase.execute(request);
+
+        var projectDto = response.getProject();
+        var issueDto = response.getIssue();
+
+        var projectVM = Project.createFrom(projectDto);
+        var issueVM = Issue.createFrom(issueDto);
+
+        vm.setProject(projectVM);
+        vm.setIssue(issueVM);
+
+        return "/issues/detail";
+    }
+
+    @ModelAttribute("editVM")
+    private EditVM createEditVM() {
+        return new EditVM();
+    }
+
+    @GetMapping("/issues/{id}/edit")
+    public String edit(
+        @PathVariable final Integer projectId,
+        @PathVariable final Integer id,
+        @ModelAttribute("editVM") final EditVM vm) {
+
+        var request = new IssueGetRequest(projectId, id);
+        var response = this.issueGetUseCase.execute(request);
+
+        var projectDto = response.getProject();
+        var issueDto = response.getIssue();
+
+        var projectVM = Project.createFrom(projectDto);
+        var issueVM = Issue.createFrom(issueDto);
+
+        vm.setProject(projectVM);
+        vm.setIssue(issueVM);
+
+        return "/issues/edit";
+    }
+
+    @PutMapping("/issues/{id}")
+    public String update(
+        @ModelAttribute("editVM") @Validated final EditVM vm,
+        final BindingResult bindingResult,
+        final RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return "/issues/edit";
+        }
+
+        try {
+            // TODO: projectIdもリクエストに含める？
+            var request = new IssueUpdateRequest(
+                Integer.parseInt(vm.getIssue().getId()),
+                vm.getIssue().getTitle(),
+                vm.getIssue().getDescription());
+
+            var response = this.issueUpdateUseCase.execute(request);
+
+            return "redirect:/projects/" + vm.getProject().getId() + "/issues/" + response.getId();
+
+        } catch (PersistenceException exception) {
+            // NOTE: 楽観ロックに失敗したらもっかい編集画面を表示させる
+            redirectAttributes.addFlashAttribute("alertMessage", exception.getMessage());
+            return "redirect:/projects/" + vm.getProject().getId() + "/issues/" + vm.getIssue().getId() + "/edit";
+        }
+
+    }
+
+    @DeleteMapping("/issues/{id}")
+    public String delete(
+        @PathVariable final Integer projectId,
+        @PathVariable final Integer id) {
+
+        var request = new IssueDeleteRequest(id);
+        this.issueDeleteUseCase.execute(request);
+
+        return "redirect:/issues";
     }
 
 }
